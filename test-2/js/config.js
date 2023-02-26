ed975ed5692287b569f4c3d8cfb8bd5b7e3ed240
Tuskajussi <jussi.tug@gmail.com>
2023-02-26 14:14:14 +0200

    AWS testing done

---
+++ test-2/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Yc9pAOUJT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5u2fqjg4r7ljg554ibejinh1ok', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://nsbroqkoj2.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
